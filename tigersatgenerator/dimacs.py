25f3306db8518fac10880f993cbe06c1c709fb76
cwebrien <cameron@brien.ca>
2019-10-06 12:56:03 -0400

    Closes #4. Add tests for DIMACS formatted string. Resolve two bugs identified by testing. Refactor to generate a DIMACS string before printing.

--- tigersatgenerator/dimacs.py
+++ tigersatgenerator/dimacs.py
@@ -20,9 +20,43 @@ def get_clauses_statistics(clauses: List[List[int]]) -> (int, int):
     return len(clauses), len(variable_set)
 
 
+def get_dimacs_cnf(clauses: List[List[int]], comment: str = None) -> str:
+    '''
+        Produces a DIMACS-formatted string of a CNF list of clauses.
+        Args:
+            clauses: The clauses to print out in CNF.
+            comment: A single-line comment; multi-line comments will break formatter
+
+        Returns:
+            String of CNF in DIMACS format.
+        '''
+
+    if "\n" in comment:
+        raise Exception("comment must be a single-line string")
+
+    result: str = ""
+
+    # Start with a comment
+    if comment is None:
+        comment = "CNF generated on " + str(datetime.datetime.now())
+    result = "c " + comment + "\n"
+
+    # Print standard CNF statistics
+    (num_clauses, num_variables) = get_clauses_statistics(clauses)
+    result = result + "p cnf " + str(num_clauses) + " " + str(num_variables) + "\n"
+
+    # Now print each clauses
+    for clause in clauses:
+        line = " ".join(str(variable) for variable in clause)
+        line = line + " 0"  # lines are 0-terminated
+        result = result + line + "\n"
+
+    return result
+
+
 def print_as_dimacs_cnf(clauses: List[List[int]], comment: str = None) -> None:
     '''
-
+    Prints a CNF formulae from a list of clauses in DIMACS-formatted text.
     Args:
         clauses: The clauses to print out in CNF.
         comment: A single-line comment; multi-line comments will break formatter
@@ -30,21 +64,5 @@ def print_as_dimacs_cnf(clauses: List[List[int]], comment: str = None) -> None:
     Returns:
         None
     '''
-
-    if comment is not None and comment.contains("\n"):
-        raise Exception("comment must be a single-line string")
-
-    # Start with a comment
-    if comment is None:
-        comment = "CNF generated on " + str(datetime.datetime.now())
-    print("c " + comment)
-
-    # Print standard CNF statistics
-    (num_clauses, num_variables) = get_clauses_statistics(clauses)
-    print("p cnf " + str(num_clauses) + " " + str(num_variables))
-
-    # Now print each clauses
-    for clause in clauses:
-        line = " ".join(str(variable) for variable in clause)
-        line = line + "0"  # lines are 0-terminated
-        print(line)
+    to_print: str = get_dimacs_cnf(clauses, comment)
+    print(to_print)
--- tigersatgenerator/tests/test_dimacs.py
+++ tigersatgenerator/tests/test_dimacs.py
@@ -4,6 +4,32 @@
         Tests for our DIMACS generator / formatter. """
 
 
+import io
+import tigersatgenerator.dimacs as dimacs
+
+
 class TestDimacsGenerator:
-    def test_get_clauses_statistics(self):
-        assert(0 == 0)
+    def test_get_dimacs_cnf(self) -> None:
+        '''
+        Some very basic sanity tests for the DIMACS string that is generated.
+        Returns:
+        '''
+        test_clauses = [[1, 2, 3], [-4, 5, -6], [7, 8, -9]]
+        dimacs_string = dimacs.get_dimacs_cnf(test_clauses, "Some comment")
+        dimacs_lines = io.StringIO(dimacs_string)
+
+        # We should start with the comment string
+        line = dimacs_lines.readline()
+        assert(line[0:2] == "c ")
+
+        # File format descriptor
+        line = dimacs_lines.readline()
+        assert(line == "p cnf 3 9\n")  # We have 3 clauses and 9 variables
+
+        # Check for proper formatting of next 3 rows
+        line = dimacs_lines.readline()
+        assert(line == "1 2 3 0\n")  # We have 3 clauses and 9 variables
+        line = dimacs_lines.readline()
+        assert(line == "-4 5 -6 0\n")  # We have 3 clauses and 9 variables
+        line = dimacs_lines.readline()
+        assert(line == "7 8 -9 0\n")  # We have 3 clauses and 9 variables
