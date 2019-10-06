4eb2bd0ed05f0697ebe5e687ad3a7eefee4b8d9e
cwebrien <cameron@brien.ca>
2019-10-05 21:34:36 -0400

    #2 Tests that we get back exceptions for invalid parameter arguments

--- tigersatgenerator/tests/test_uniformksat_generator.py
+++ tigersatgenerator/tests/test_uniformksat_generator.py
@@ -3,6 +3,7 @@
 """ tigersatgenerator/tests/test_uniformksat_generator.py
         Tests for our random k-SAT generator. """
 
+import pytest
 import tigersatgenerator.uniformksat.generator as ksatgen
 
 
@@ -25,3 +26,13 @@ class TestUniformKSat:
             assert(num_clauses == len(clauses))
             for clause in clauses:
                 assert(k == len(clause))
+
+        # Exception cases as (k, num_clauses, num_variables)
+        test_cases = [
+            (-5, 5, 3),  # negative k
+            (2, -5, 3),  # negative num_clauses
+            (5, 3, 4)    # insufficient variables to support k
+        ]
+        for (k, num_clauses, num_variables) in test_cases:
+            with pytest.raises(Exception):
+                clauses = ksatgen.random_ksat_clauses(k, num_clauses, num_variables)
